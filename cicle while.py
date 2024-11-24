7f45ac95675720266b9f22b3ca8f854652d7195b
ljusien <ljusien777@gmail.com>
2024-11-25 00:36:57 +0300

    Add files via upload

---
+++ cicle while.py
@@ -0,0 +1,32 @@
+l= [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index<len(l):
+    print(l[index])
+    index += 1
+    break
+    index = 1
+while index < len(l):
+        print(l[index])
+        index += 1
+        break
+index = 2
+while index < len(l):
+        print(l[index])
+        index += 1
+        break
+index = 3
+while index < len(l):
+        print(l[index])
+        index += 1
+        break
+index = 5
+while index < len(l):
+        print(l[index])
+        index += 1
+        break
+
+
+
+
+
+
